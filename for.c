6f5a9c2b0726c4ee6fbc540d47211f49bfb6908c
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-10-06 16:17:03 -0700

    make function

--- for.c
+++ for.c
@@ -1,8 +1,18 @@
 #include <stdio.h> 
 
+
+void meow(void)
+{
+    printf("meow\n");
+}
+
 int main(void)
 {
+    // for(int i = 0; i < 3; i++) 
+    //     printf("meow\n");
     for(int i = 0; i < 3; i++) {
-        printf("meow\n");
+        meow();
     }
+    
 }
+
