d77b662e4c83ce7f91c18fbb94a9f972b2f91052
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-10-06 17:29:23 -0700

    coments and difference

--- mario.c
+++ mario.c
@@ -8,8 +8,21 @@ int main(void)
     //     printf("?");
     // }
     // printf("\n");
-    const int n = get_int("Size: ");
+    // int n = get_int("Size: ");
+    // while (n < 1)
+    // {
+    //     n = get_int("Size: ");
+    // }
+    // Prompt user for positive integer
+    int n;
+    do 
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
     
+
+    // Print an n-by-n grid of bricks
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
