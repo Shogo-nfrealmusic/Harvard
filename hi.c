5a8a377626191aefbaf8037fb9ef19dfa177c739
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-10-17 22:02:25 -0700

    concept of byte and bite

---
+++ hi.c
@@ -0,0 +1,8 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = "Hi!";
+    printf("%s\n", s);
+}
--- scores.c
+++ scores.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int N = 3;
+
 int main(void)
 {
-    int scores[3];
-    scores[0] = get_int("Score: ");
-    scores[1] = get_int("Score: ");
-    scores[2] = get_int("Score: ");
+    int scores[N];
+    for (int i = 0; i < N; i++){
+        scores[i] = get_int("Score: ");
+    }
 
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0 );
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) N);
 }
