159405b91d2169c4850a1708f2b653b0f1de8870
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-10-19 13:58:03 -0700

    data structure

--- week3/phonebook.c
+++ week3/phonebook.c
@@ -2,7 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
 int main(void)
 {
-    
+    person people[3];
+    people[0].name = "Shogo";
+    people[0].number = "+1-000-000-0000";
+    people[1].name = "Haruka";
+    people[1].number = "+1-111-111-1111";
+    people[2].name = "Harvard";
+    people[2].number = "+1-222-222-2222";
+
+    string name = get_string("Name: ");
+
+    for (int i =0; i < 3; i++)
+    {
+        if (strcmp(people[i].name, name) == 0)
+        {
+            printf("Found %s\n", people[i].number);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
