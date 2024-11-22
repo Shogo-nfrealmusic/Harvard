b34d50be9296dba6126e57ec246b3742ec1b47cc
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-11-22 11:06:13 -0800

    how to combine sql in flask

--- week9/hello/app.py
+++ week9/hello/app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)   
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html") 
+    if request.method == "POST":
+        name = request.form.get("name")
+        return render_template("greet.html", name=name)
+    else:
+        return render_template("index.html") 
 
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", "world")
-    return render_template("greet.html", name=name)
+
+    
 
  
